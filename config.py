f3eac729ddcad568c2a25d9d5dd48324e057b981
Yogender <129525898+yogender747@users.noreply.github.com>
2025-01-22 11:18:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @BLURYF4CE
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7208692651:AAHqBD1nkTCKC7j4mqrSrVI7iiDBk74_hNI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Rishi747")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1319994892")
+
+
+
+
+
+
+
+  
